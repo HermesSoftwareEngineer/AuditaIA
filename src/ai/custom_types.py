4cf852932774dd962cbad480bc2300095ec8dba7
Hermes <hermesbarbosa9@gmail.com>
2025-07-16 18:33:09 -0300

    feat: inicar ajustes do grafo de planejador pro chat

--- src/ai/custom_types.py
+++ src/ai/custom_types.py
@@ -1,6 +1,21 @@
-from typing import Annotated
+from typing import Annotated, List, Tuple
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
+import operator
+from pydantic import BaseModel, Field
 
 class State(TypedDict):
     messages: Annotated[list[str], add_messages]
+
+class PlanExecute(TypedDict):
+    input: str
+    plan: List[str]
+    past_steps: Annotated[List[Tuple], operator.add]
+    response: str
+
+class Plan(BaseModel):
+    """Planejamento pra seguir no futuro"""
+
+    steps: List[str] = Field(
+        description="diferentes etapas a seguir, devem estar em ordem classificada"
+    )
--- src/ai/prompts.py
+++ src/ai/prompts.py
@@ -24,3 +24,20 @@ prompt_assistente = ChatPromptTemplate.from_messages(
     ]
 )
 
+planner_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Elabore um plano de utilização de ferramentas pra conseguir os dados necessário para cumprir o objetivo informado.
+            Ferramentas disponíveis:
+            - tool_coletar_dados_repasse: coleta dados de repasse. Parametros necessários: codigoCliente: int, codigoImovel: int, ano: int, mes: int
+            - tool_pesquisar_clientes: pesquisa o cliente e descobre o código dele. Parametros necessários: textoPesquisa: str
+            - tool_retornar_imoveis_do_locador: 
+            - tool_retornar_contratos_do_locador
+            - tool_retornar_contratos_do_locatario
+            """
+        ),
+        MessagesPlaceholder(variable_name="input")
+    ]
+)
